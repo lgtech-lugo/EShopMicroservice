2a65905ca08786957db5aeba3a977d26445187aa
lugo <lucas.goffinet@lgtech.be>
2024-09-05 16:00:03 +0200

    feat: use building blocks handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,7 +6,7 @@ namespace Catalog.API.Products.CreateProduct;
 internal record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 internal record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {

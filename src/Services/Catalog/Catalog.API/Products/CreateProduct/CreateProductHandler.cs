f5e03d6585ee9ec3eefd4fbbabd754e1c15cabdf
lugo <lucas.goffinet@lgtech.be>
2024-09-05 15:59:22 +0200

    feat: create CQRS building blocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,9 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
-internal record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+internal record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 internal record CreateProductResult(Guid Id);
 
 internal class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>

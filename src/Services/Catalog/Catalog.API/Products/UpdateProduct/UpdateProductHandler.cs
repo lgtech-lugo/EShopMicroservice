ecd662a577821c7b09c8904b4dd43b39bc11d3f8
lugo <lucas.goffinet@lgtech.be>
2024-09-10 17:07:05 +0200

    feat: UpdateProductEndpoint.cs

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+internal record UpdateProductCommand(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<UpdateProductResult>;
+
+internal record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler(IDocumentSession session): ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        Debug.Assert(product != null, nameof(product) + " != null");
+        
+        product.Name = command.Name;
+        product.Category = command.Category;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+        return new UpdateProductResult(true);
+    }
+}

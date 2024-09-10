156ea94f962e21a454bdc265f5c1ea7224ca6e82
lugo <lucas.goffinet@lgtech.be>
2024-09-10 16:01:20 +0200

    feat: GetProductByCategoryHandler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+internal record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+internal record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger): IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryHandler");
+        var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+        return new GetProductByCategoryResult(products);
+    }
+}

5af9ca2eaa8fd8e088076e682641215a18eef55a
lugo <lucas.goffinet@lgtech.be>
2024-09-10 17:11:23 +0200

    feat: handler

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+internal record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+internal record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(IDocumentSession session): ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+        return new DeleteProductResult(true);
+    }
+}

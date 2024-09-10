088940156e7afdfba2dfc3d567db46c460aaf3ee
lugo <lucas.goffinet@lgtech.be>
2024-09-10 16:03:32 +0200

    feat: GetProductByCategoryEndpoint.cs

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+internal record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint: ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            var response = result.Adapt<GetProductByCategoryResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProductByCategory")
+            .Produces<GetProductByCategoryResponse>()
+            .Produces(StatusCodes.Status404NotFound)
+            .WithSummary("Get Product By Category")
+            .WithDescription("Get Product By Category");
+    }
+}

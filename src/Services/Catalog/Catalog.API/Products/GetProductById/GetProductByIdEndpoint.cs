3963d9a03d4e606e7ea22f901a8d0005f079f51a
lugo <lucas.goffinet@lgtech.be>
2024-09-10 15:52:38 +0200

    feat: GetProductByIdEndpoint.cs

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,21 @@
-﻿namespace Catalog.API.Products.GetProductById;
+﻿using Catalog.API.Products.GetProducts;
 
-internal class GetProductByIdEndpoint
+namespace Catalog.API.Products.GetProductById;
+
+internal record GetProductByIdResponse(Guid Id);
+public class GetProductByIdEndpoint: ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProductById")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+    }
 }

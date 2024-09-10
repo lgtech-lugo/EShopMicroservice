874f25da876db091c320f5e0ddfea29517879bc9
lugo <lucas.goffinet@lgtech.be>
2024-09-10 15:28:13 +0200

    feat: GetProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProducts;
+internal record GetProductsQuery : IQuery<GetProductsResult>;
+internal record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger): IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Handle called iwth {@Query}", query);
+        var products =  await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
+}

71692d0f4012bda6a65060a912d7ec6094c3786c
lugo <lucas.goffinet@lgtech.be>
2024-09-17 13:22:17 +0200

    feat: clean code

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using Catalog.API.Models;
-
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 internal record CreateProductCommand(
     string Name,

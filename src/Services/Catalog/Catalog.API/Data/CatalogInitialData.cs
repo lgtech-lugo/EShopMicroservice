18a11bb0ac6fd7e5265de5f24eb7364fad4d31c3
lugo <lucas.goffinet@lgtech.be>
2024-09-17 15:07:38 +0200

    feat: add seeder

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,25 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data;
+
+public class CatalogInitialData: IInitialData
+{
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        await using var session = store.LightweightSession();
+        if (await session.Query<Product>().AnyAsync(cancellation))
+            return;
+        
+        session.Store(new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Product 1",
+            Description = "Description 1",
+            Price = 100,
+            Category = ["Category 1"],
+            ImageFile = "Image 1"
+        });
+
+        await session.SaveChangesAsync(cancellation);
+    }
+}

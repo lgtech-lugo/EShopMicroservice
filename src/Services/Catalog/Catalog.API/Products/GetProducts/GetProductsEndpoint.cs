8428479627505c6b16de522f9ea64881f5dcbb91
lugo <lucas.goffinet@lgtech.be>
2024-09-17 15:12:04 +0200

    feat: implement pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -2,18 +2,21 @@
 
 namespace Catalog.API.Products.GetProducts;
 
+internal record GetProductRequest(int? PageNumber = 1, int? PageSize = 10);
+
 internal record GetProductResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
-        {
-            var result = await sender.Send(new GetProductsQuery());
-            var response = result.Adapt<GetProductResponse>();
-            return Results.Ok(response);
-        })
+        app.MapGet("/products", async ([AsParameters] GetProductRequest request, ISender sender) =>
+            {
+                var query = request.Adapt<GetProductsQuery>();
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
             .WithName("GetProducts")
             .Produces<GetProductResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,15 +1,17 @@
-﻿using Catalog.API.Models;
+﻿namespace Catalog.API.Products.GetProducts;
 
-namespace Catalog.API.Products.GetProducts;
-internal record GetProductsQuery : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
+
 internal record GetProductsResult(IEnumerable<Product> Products);
 
-internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger): IQueryHandler<GetProductsQuery, GetProductsResult>
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
         logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
-        var products =  await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
         return new GetProductsResult(products);
     }
 }
